82c1bd944efbab977913374ff114fc30937da857
pez <perr@arch.com>
2024-06-05 15:14:46 +0100

    Initial Commit

---
+++ cost.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+from pathlib import Path
+
+DIR = Path("/home/$USER/Documents/Competitions/Shell/dataset/")
+
+class Cost:
+    def __init__(self):
+        print("Cost Class!")
+        return 
+
+def main():
+    Cost()
+    return 0
+
+if __name__ == '__main__':
+    main()
