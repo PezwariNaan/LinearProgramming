8e88a278b798b3129f6eedf6a1325fa0265a23e1
pez <perr@arch.com>
2024-06-06 14:48:17 +0100

    Buy, Sell, Insure and Maintain functions implemented. Need to decide how required distance is calculated. Possibly refactor to better reflect the hierarchy of year -> fleet -> vehicles -> use.

--- cost.py
+++ cost.py
@@ -25,11 +25,6 @@ class DF:
             setattr(self, df_name, df)
         return 
 
-    def list_df_keys(self):
-        attributes = [attr for attr in dir(self) if not callable(getattr(self, attr)) and not attr.startswith("__")]
-        for attr in attributes:
-            print(attr)
-        return
 
 ###################################################################
 class Vehicle:
@@ -47,45 +42,140 @@ class Vehicle:
 ###################################################################
 class Model:
     def __init__(self, dataframes: DF):
+        # Easy Access to dataframes
+        self.vehicles_df = dataframes.vehicles
+        self.vehicle_fuels_df = dataframes.vehicle_fuels
+        self.fuels_df = dataframes.fuels
+        self.demand_df = dataframes.demand
+        self.cost_profiles_df = dataframes.cost_profiles
+        self.carbon_emissions_df = dataframes.carbon_emissions
+
         # Set inital conditions
+        self.current_year = 2023
         self.fleet = Counter({}) # Dictionary of Vehicles & Number Owned
-        self.purchase_costs = 0
-        self.dataframes = dataframes
+        self.costs = 0
+        self.emissions = 0
         return
 
-    def purchase_vehicle(self, vehicle_ID:str, fuel_type:str):
-        df_vehicles = self.dataframes.vehicles
-        vehicle = Vehicle(vehicle_ID, fuel_type)
-        vehicle_details = df_vehicles[df_vehicles['ID'] == vehicle_ID]
-        self.purchase_costs += vehicle_details['Cost ($)'].values[0]
-        self.fleet.update({vehicle : 1})
+    def purchase_vehicle(self, vehicle_ID: str, fuel_type: str):
+        try:
+            vehicle_details = self.vehicles_df[self.vehicles_df['ID'] == vehicle_ID]
+
+            if vehicle_details.empty:
+                print(f"Vehicle {vehicle_ID} not found.")
+                return 
+
+            vehicle = Vehicle(vehicle_ID, fuel_type)
+            self.costs += vehicle_details['Cost ($)'].values[0]
+            self.fleet.update({vehicle : 1})
+            return 
+
+        except Exception as e:
+            print(f"Error: {e}")
+            return 
+
+    def sell_vehicle(self, vehicle_ID: str, fuel_type: str):
+        try:
+            vehicle = Vehicle(vehicle_ID, fuel_type)
+            if vehicle in self.fleet:
+                vehicle_details = self.vehicles_df[self.vehicles_df['ID'] == vehicle_ID]
+
+                vehicle_purchase_year = vehicle_details['Year'].values[0]
+
+                vehicle_age = self.current_year - vehicle_purchase_year
+
+                vehicle_purchase_price = vehicle_details['Cost ($)'].values[0]
+
+                # Retrieve the devalue rate from the cost_profiles dataframe
+                # Based on the age of the vehicle. 
+                resale_rate = self.cost_profiles_df[self.cost_profiles_df\
+                        ['End of Year'] == vehicle_age + 1]\
+                        ['Resale Value %'].item()
+
+                resale_value = (resale_rate / 100) * vehicle_purchase_price
+                self.costs -= resale_value
+            else:
+                print("Vehicle is not in Fleet")
+                return 
+
+        except Exception as e:
+            print(f"Error: {e}")
+            return 
+
+    def use_vehicle(self, vehicle_ID: str, fuel_type: str, distance: int):
+        """
+        Distance 
+        Fuel Used        = Consumption Rate * Distance
+        Fuel Cost        = Fuel Used * Fuel Cost per Unit
+        Carbon Emissions = Fuel Used * Carbon Emissions per Unit
+        """
+        # Fuel details
+        
         return 
-         
+
+    def insure(self, vehicle_ID: str):
+        vehicle_details = self.vehicles_df[self.vehicles_df['ID'] == vehicle_ID]
+
+        vehicle_purchase_year = vehicle_details['Year'].values[0]
+
+        vehicle_age = self.current_year - vehicle_purchase_year
+
+        vehicle_purchase_price = vehicle_details['Cost ($)'].values[0]
+
+        # Retrieve the insurance rate from the cost_profiles dataframe
+        # Based on the age of the vehicle. 
+        insurance_rate = self.cost_profiles_df[self.cost_profiles_df\
+                ['End of Year'] == vehicle_age + 1]\
+                ['Insurance Cost %'].item()
+
+        insurance_cost = (insurance_rate / 100) * vehicle_purchase_price
+        return
+        
+    def maintain(self, vehicle_ID: str):
+        vehicle_details = self.vehicles_df[self.vehicles_df['ID'] == vehicle_ID]
+
+        vehicle_purchase_year = vehicle_details['Year'].values[0]
+
+        vehicle_age = self.current_year - vehicle_purchase_year
+
+        vehicle_purchase_price = vehicle_details['Cost ($)'].values[0]
+
+        # Retrieve the maintenance rate from the cost_profiles dataframe
+        # Based on the age of the vehicle. 
+        maintenance_rate = self.cost_profiles_df[self.cost_profiles_df\
+                ['End of Year'] == vehicle_age + 1]\
+                ['Maintenance Cost %'].item()
+
+        maintenance_cost = (maintenance_rate / 100) * vehicle_purchase_price
+        return
+
+##################### CLASS HELPER FUNCTIONS #######################
     def list_fleet(self):
         print("Vehicles Currently in Fleet:")
-        for vehicle in self.fleet.items():
-            print(f"{vehicle[0].ID}: {vehicle[1]} ({vehicle[0].fuel_type})")
+        for vehicle, count in self.fleet.items():
+            print(f"{vehicle.ID}: {count} ({vehicle.fuel_type})")
         return
+
+    def tidy_fleet(self):
+        # Remove any item in self.fleet that's value is 0
+        self.fleet = Counter({k: v for k, v in self.fleet.items() if v != 0})
+        return 
 
 ####################################################################
 def main():
-
     dataframes = DF()
     model = Model(dataframes)
-    model.purchase_vehicle('BEV_S2_2023', 'Electrical')
-    model.purchase_vehicle('BEV_S2_2023', 'Electrical')
-    model.purchase_vehicle('BEV_S4_2023', 'Electrical')
-    model.purchase_vehicle('BEV_S4_2023', 'Banana')
-    model.list_fleet()
+
 
     """
-    Variables to Consider / Implement:
-        start_year = 2023
-        current_year = year
-        end_year = 2038
+    model.purchase_vehicle('BEV_S2_2023', 'Electrical')
+    model.purchase_vehicle('BEV_S2_2023', 'Electrical')
+    model.sell_vehicle('BEV_S2_2023', 'Electrical')
+    model.sell_vehicle('BEV_S2_2023', 'Electrical')
 
-        cost_of_purchasing_vehicles = vehicle_price * number_of_vehicles
-        cost of insurace = age * insurance(age)
+    Considerations | Implementations:
+        end_year = 2038
+        How to calculate distance? 
     """
 
 if __name__ == '__main__':
