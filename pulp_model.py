3cd25887358c04749708cd89b72cfad1e15edf54
pez <perr@arch.com>
2024-06-26 16:32:17 +0100

    Added Vehicle Class and refactored. Everything seems legit. Need to add demand then will run it.

--- pulp_model.py
+++ pulp_model.py
@@ -5,6 +5,25 @@ import pulp as pl
 import numpy as np
 
 class Model:
+    class Vehicle:
+        def __init__(self, id, fuel):
+            self.id = id
+            self.fuel = fuel
+
+        def __hash__(self):
+            return hash((self.id, self.fuel))
+
+        def __str__(self):
+            return f"{self.id}_{self.fuel}"
+
+        def __repr__(self): 
+            return self.__str__()
+
+        def __eq__(self, other):
+            if isinstance(other, Model.Vehicle):
+                return self.id == other.id and self.fuel == other.fuel
+            return False
+
     # Load data
     def __init__(self):
         # Cost profiles isn't returned because we don't need it yet
@@ -16,14 +35,22 @@ class Model:
         self.carbon_emissions = pd.read_csv(dir + 'carbon_emissions.csv')
         self.cost_profiles = pd.read_csv(dir + 'cost_profiles.csv')
         self.year = 2023
-        return 
 
     def create_problem(self):
         problem = pl.LpProblem(f"Fleet_Optimization_{self.year}", pl.LpMinimize)
         
         # Define decision variables
-        list_of_vehicles = self.vehicles[self.vehicles['Year'] == \
-                self.year]['ID'].values
+        list_of_vehicles = []
+
+        #fueltype = {ID: {fuel: consumption_rate}}
+        vehicle_fueltype = self.vehicle_fuels[self.vehicle_fuels['ID'].str.contains(str(self.year))].groupby('ID').apply(
+            lambda x: x.set_index('Fuel')['Consumption (unit_fuel/km)'].to_dict(),
+            include_groups = False
+        ).to_dict()
+
+        for vehicle in vehicle_fueltype:
+            for fuel in vehicle_fueltype[vehicle]:
+                list_of_vehicles.append(self.Vehicle(vehicle, fuel))
 
         number_purchased = {vehicle: pl.LpVariable(f"num_purchased_{vehicle}", lowBound=0, cat="Integer") \
                 for vehicle in list_of_vehicles}
@@ -36,12 +63,6 @@ class Model:
 
         vehicle_cost = self.vehicles[self.vehicles['Year'] == \
                 self.year].set_index('ID')['Cost ($)'].to_dict()
-
-        #fueltype = {ID: {fuel: consumption_rate}}
-        vehicle_fueltype = self.vehicle_fuels[self.vehicle_fuels['ID'].str.contains(str(self.year))].groupby('ID').apply(
-            lambda x: x.set_index('Fuel')['Consumption (unit_fuel/km)'].to_dict(),
-            include_groups = False
-        ).to_dict()
 
         fuel_price = self.fuels[self.fuels['Year'] == self.year]\
                 .set_index('Fuel')['Cost ($/unit_fuel)'].to_dict()
@@ -56,14 +77,14 @@ class Model:
 
         vehicle_age = (self.vehicles.set_index('ID')['Year'] - self.year + 1).to_dict()
 
-        cost_to_insure   = {vehicle: vehicle_cost[vehicle] * self.cost_profiles[self.cost_profiles['End of Year'] == \
-                vehicle_age[vehicle]]['Insurance Cost %'].item() / 100 for vehicle in list_of_vehicles}
+        cost_to_insure   = {vehicle: vehicle_cost[vehicle.id] * self.cost_profiles[self.cost_profiles['End of Year'] == \
+                vehicle_age[vehicle.id]]['Insurance Cost %'].item() / 100 for vehicle in list_of_vehicles}
 
-        cost_to_maintain = {vehicle: vehicle_cost[vehicle] * self.cost_profiles[self.cost_profiles['End of Year'] == \
-                vehicle_age[vehicle]]['Maintenance Cost %'].item() / 100 for vehicle in list_of_vehicles} 
+        cost_to_maintain = {vehicle: vehicle_cost[vehicle.id] * self.cost_profiles[self.cost_profiles['End of Year'] == \
+                vehicle_age[vehicle.id]]['Maintenance Cost %'].item() / 100 for vehicle in list_of_vehicles} 
 
-        sale_price = {vehicle: vehicle_cost[vehicle] * self.cost_profiles[self.cost_profiles['End of Year'] == \
-                vehicle_age[vehicle]]['Resale Value %'].item() / 100 for vehicle in list_of_vehicles} 
+        sale_price = {vehicle: vehicle_cost[vehicle.id] * self.cost_profiles[self.cost_profiles['End of Year'] == \
+                vehicle_age[vehicle.id]]['Resale Value %'].item() / 100 for vehicle in list_of_vehicles} 
         
         # Ensure distance vehicle travels is less than the yearly_range
         vehicle_max_range = self.vehicles[self.vehicles['Year'] == self.year]\
@@ -77,19 +98,20 @@ class Model:
             problem += number_sold[vehicle] <= number_purchased[vehicle], f"Sell_Constraint_{vehicle}"
 
          # Emission constraint
-        total_emissions = pl.lpSum([number_used[vehicle] * vehicle_fueltype[vehicle][fuel] * fuel_emission_rate[fuel] 
-                                    for vehicle in list_of_vehicles for fuel in vehicle_fueltype[vehicle]])
+        total_emissions = pl.lpSum([number_used[vehicle] * vehicle_fueltype[vehicle.id][vehicle.fuel] * fuel_emission_rate[vehicle.fuel] 
+                                    for vehicle in list_of_vehicles])
         problem += total_emissions <= carbon_limit, "Total_Emission_Constraint"
 
+        # Demand must reach 0
         # Distance bucket >= demand bucket
 
         # Size bucket == size bucket
 
         # Must sell vehicle at ten year point
         for vehicle in list_of_vehicles:
-                    if vehicle_age[vehicle] >= 10:
+                    if vehicle_age[vehicle.id] >= 10:
                         problem += number_sold[vehicle] ==\
-                                number_purchased[vehicle], f"Age_Limit_Constraint_{vehicle}"
+                                number_purchased[vehicle], f"Age_Limit_Constraint_{vehicle.id}"
 
 
         # Must sell less than 20% of fleet per year
@@ -97,8 +119,9 @@ class Model:
                 pl.lpSum(number_purchased.values()), "Fleet_Sell_Constraint"
 
         # Objective Function
-        total_cost = pl.lpSum([number_purchased[vehicle] * vehicle_cost[vehicle] + 
-                                   number_used[vehicle] * (sum(fuel_price[fuel] * vehicle_fueltype[vehicle][fuel] for fuel in vehicle_fueltype[vehicle]) + cost_to_maintain[vehicle]) - 
+        total_cost = pl.lpSum([number_purchased[vehicle] * vehicle_cost[vehicle.id] + 
+                                   number_used[vehicle] * (sum(fuel_price[vehicle.fuel] * vehicle_fueltype[vehicle.id][vehicle.fuel] \
+                                           for vehicle in list_of_vehicles) + cost_to_maintain[vehicle] + cost_to_insure[vehicle]) - \
                                    number_sold[vehicle] * sale_price[vehicle] for vehicle in list_of_vehicles])
 
         problem += total_cost, "Total Cost"
