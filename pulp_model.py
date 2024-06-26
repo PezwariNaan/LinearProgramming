bc37cff911ee42c1a29bc2e257af542bf5c4b60e
pez <perr@arch.com>
2024-06-26 08:53:15 +0100

    Majority of variables defined. Several constraints added to problem.

--- pulp_model.py
+++ pulp_model.py
@@ -25,44 +25,68 @@ class Model:
         list_of_vehicles = self.vehicles[self.vehicles['Year'] == \
                 self.year]['ID'].values
 
-        vehicle_cost             = self.vehicles[self.vehicles['Year'] == \
+        number_purchased = {vehicle: pl.LpVariable(f"num_purchased_{vehicle}", lowBound=0, cat="Integer") \
+                for vehicle in list_of_vehicles}
+        
+        number_used = {vehicle: pl.LpVariable(f"num_used_{vehicle}", lowBound= 0, cat="Integer") \
+                for vehicle in list_of_vehicles}
+
+        number_sold = {vehicle: pl.LpVariable(f"num_sold_{vehicle}", lowBound=0, cat="Integer") \
+                for vehicle in list_of_vehicles}
+
+        vehicle_cost = self.vehicles[self.vehicles['Year'] == \
                 self.year].set_index('ID')['Cost ($)'].to_dict()
-        print(vehicle_cost)
-        exit()
 
-        number_purchased         = None # Number Purchased
-        number_used              = None # Second Variable 
-        number_sold              = None # Third Variable
+        #fueltype = {ID: {fuel: consumption_rate}}
+        vehicle_fueltype = self.vehicle_fuels[self.vehicle_fuels['ID'].str.contains(str(self.year))].groupby('ID').apply(
+            lambda x: x.set_index('Fuel')['Consumption (unit_fuel/km)'].to_dict(),
+            include_groups = False
+        ).to_dict()
 
-        vehicle_name             = None 
-        vehicle_fueltype         = None
-        vehicle_range            = None
+        fuel_price  = self.fuels[self.fuels['Year'] == self.year]\
+                .set_index('Fuel')['Cost ($/unit_fuel)'].to_dict()
 
-        fuel_price               = 0
-        fuel_consumption_rate    = 0
-        fuel_emission_rate       = 0
-
-        cost_to_insure           = None # Probably a function 
-        cost_to_maintain         = None # Probably a function 
+        fuel_emission_rate = self.fuels[self.fuels['Year'] == self.year]\
+                .set_index('Fuel')['Emissions (CO2/unit_fuel)'].to_dict()
         
-        distance_covered         = 0
-        carbon_emissions         = self.carbon_emissions[self.carbon_emissions['Year'] == \
+        distance_covered = 0
+
+        carbon_limit = self.carbon_emissions[self.carbon_emissions['Year'] == \
                 self.year]['Carbon emission CO2/kg'].item()
-        
 
+        cost_to_insure   = None # Probably a function 
+        cost_to_maintain = None # Probably a function 
+        sale_price       = None # Probably a function 
+        
         # Set up constraints
+        """
         problem += distance_covered * fuel_emission_rate  * \
-                fuel_consumption_rate  <= carbon_emissions
+                fuel_consumption_rate  <= carbon_limit
+        """
+        # Must sell less than 20% of fleet per year
         percentage_of_fleet_sold = None
+
+        # Make sure vehicles used and sold are owned
+        for vehicle in list_of_vehicles:
+            problem += number_used[vehicle] <= number_purchased[vehicle], f"Use_Constraint_{vehicle}"
+            problem += number_sold[vehicle] <= number_purchased[vehicle], f"Sell_Constraint_{vehicle}"
+
+        self.number_purchased = number_purchased
+        self.number_used = number_used
+        self.number_sold = number_sold
+
+        # Ensure distance vehicle travels is less than the yearly_range
+        vehicle_max_range = self.vehicles[self.vehicles['Year'] == self.year]\
+                .set_index('ID')['Yearly range (km)'].to_dict()
+        
+        print(problem)
+        exit()
 
         # Define objective function
         total_cost               = None
 
         return problem
     
-    def update_fleet(self):
-        pass
-
     # Implement rolling horizon approach
     def rolling_horizon_optimization(self, start_year, end_year, horizon):
         fleet = {} # Dictionary  = Vehicle : Count
@@ -78,3 +102,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
